d4cb29249acbebcd720f451f3b64a099bf4ac4a6
Filipe Macedo <devmacedo@gmail.com>
2020-04-08 03:14:29 -0300

    feat: create component products list

--- src/components/ProductDescription/index.tsx
+++ src/components/ProductDescription/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './styles/product-description.styles.scss';
+import './product-description.styles.scss';
 
 export interface ProductDescriptionProps {
   description: string;
---
+++ src/components/ProductsList/index.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/jsx-max-depth */
+import React from 'react';
+import ProductPicture from '../ProductPicture';
+import ProductInformations from '../ProductInformations';
+import ProductLocation from '../ProductLocation';
+import MacbookImage from '../../assets/images/macbook.webp';
+
+import "./products-list.styles.scss";
+
+interface Props {
+  products: any[];
+}
+
+const ProductsList: React.FC<Props> = () => {
+  return (
+    <ol className="products-list">
+      <li className="products-list__product">
+        <ProductPicture alt="Um macbook novo!" src={MacbookImage} />
+        <ProductInformations description="Lorem ipsum" value="1998" />
+        <ProductLocation location="Canada" />
+      </li>
+    </ol>
+  );
+};
+
+export default React.memo(ProductsList);
