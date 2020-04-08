85cc631f8f842a40bc4d573ec3cd23218662b884
Filipe Macedo <devmacedo@gmail.com>
2020-04-08 01:30:05 -0300

    feat: add products search component

---
+++ src/components/ProductsSearch/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import SearchIcon from '../../assets/icons/ic_Search.png';
+
+import './products-search.styles.scss';
+
+const ProductsSearch: React.FC = () => {
+  return (
+    <form className="products-search">
+      <input placeholder="Nunca dejes de buscar" type="search" />
+      <button type="submit">
+        <img src={SearchIcon} />
+      </button>
+    </form>
+  );
+};
+
+export default ProductsSearch;
