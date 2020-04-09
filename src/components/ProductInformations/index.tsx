9e21433fe433572d0c5da4170c25e493c9e62acf
Filipe Macedo <devmacedo@gmail.com>
2020-04-08 21:39:42 -0300

    refactor: change tag of product name

--- src/components/ProductInformations/index.tsx
+++ src/components/ProductInformations/index.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import ProductPrice, { ProductPriceProps } from '../ProductPrice';
-import ProductDescription, {
-  ProductDescriptionProps,
-} from '../ProductDescription';
 
-import "./product-informations.styles.scss"
+import './product-informations.styles.scss';
 
 interface ProductInformationsProps {
   className?: string;
+  name: string;
 }
 
-type Props = ProductDescriptionProps &
-  ProductInformationsProps &
-  ProductPriceProps;
+type Props = ProductPriceProps & ProductInformationsProps;
 
 const ProductInformations: React.FC<Props> = ({
-  className,
-  description,
   value,
   symbolCurrency,
-  hideShippingIcon
+  hideShippingIcon,
+  name,
 }) => (
-  <div className={`product__informations ${className}`}>
-    <ProductPrice hideShippingIcon={hideShippingIcon} symbolCurrency={symbolCurrency} value={value} />
-    <ProductDescription description={description} />
+  <div className="product__informations">
+    <ProductPrice
+      hideShippingIcon={hideShippingIcon}
+      symbolCurrency={symbolCurrency}
+      value={value}
+    />
+    <p className="product__location">Canada</p>
+    <h2 className="product__name">{name}</h2>
   </div>
 );
 
