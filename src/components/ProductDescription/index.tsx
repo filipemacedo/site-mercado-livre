8ba0e2d322e17fbdb235324b70d9d7c8d8845327
Filipe Macedo <devmacedo@gmail.com>
2020-04-09 01:38:13 -0300

    feat: add title in the product description

--- src/components/ProductDescription/index.tsx
+++ src/components/ProductDescription/index.tsx
@@ -4,10 +4,15 @@ import './product-description.styles.scss';
 
 export interface ProductDescriptionProps {
   description: string;
+  title: string;
 }
 
-const ProductDescription: React.FC<ProductDescriptionProps> = ({ description }) => (
+const ProductDescription: React.FC<ProductDescriptionProps> = ({
+  description,
+  title,
+}) => (
   <div className="product__description">
+    <h2>{title}</h2>
     <p>{description}</p>
   </div>
 );
