0800a43f935b419d6b5f9cd677f9bf93afa3bc35
Filipe Macedo <devmacedo@gmail.com>
2020-04-09 00:45:35 -0300

    feat: create picture large mode

--- src/components/ProductPicture/index.tsx
+++ src/components/ProductPicture/index.tsx
@@ -5,10 +5,15 @@ import './product-picture.styles.scss';
 interface Props {
   src: string;
   alt: string;
+  large?: boolean;
 }
 
-const ProductPicture: React.FC<Props> = ({ src, alt }) => (
-  <img alt={alt} className="product__picture" src={src} />
+const ProductPicture: React.FC<Props> = ({ src, alt, large = false }) => (
+  <img
+    alt={alt}
+    className={`product__picture ${large ? 'product__picture--large' : ''}`}
+    src={src}
+  />
 );
 
 export default ProductPicture;
