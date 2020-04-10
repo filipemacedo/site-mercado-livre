a502ca24f08acbd1a8bfa020ab862ea383fccaa0
Filipe Macedo <devmacedo@gmail.com>
2020-04-10 08:32:13 -0300

    test: ensure ProductPrice hide the shipping icon when hideShippingIcon is true

--- src/components/ProductPrice/__tests__/ProductPrice.spec.tsx
+++ src/components/ProductPrice/__tests__/ProductPrice.spec.tsx
@@ -10,15 +10,23 @@ describe('ProductPrice Component', () => {
 		expect(app.find('.price__symbol.margin-right--5')).toHaveLength(1);
 		expect(app.find('.price__symbol.margin-right--5').text()).toBe('R$');
 		expect(app.find('.price__value')).toHaveLength(1);
-		expect(app.find('.price__value').text()).toEqual("100");
+		expect(app.find('.price__value').text()).toEqual('100');
 		expect(app.find('img.shipping-icon.margin-left--10')).toHaveLength(1);
- });
- 
- it('should render ProductPrice with custom props', () => {
+	});
+
+	it('should render ProductPrice with custom props', () => {
 		const app = mount(<ProductPrice value={100} symbolCurrency="$" />);
 
 		expect(app.find('.price__symbol.margin-right--5').text()).toBe('$');
 		expect(app.find('.price__value')).toHaveLength(1);
-		expect(app.find('.price__value').text()).toEqual("100");
+		expect(app.find('.price__value').text()).toEqual('100');
+	});
+
+	it('should render ProductPrice and hide the shipping icon when hideShippingIcon is true', () => {
+		const app = mount(<ProductPrice value={100} hideShippingIcon />);
+
+		expect(app.find('.price__value').text()).toEqual('100');
+		expect(app.find('.price__value')).toHaveLength(1);
+		expect(app.find('img.shipping-icon.margin-left--10')).toHaveLength(0);
 	});
 });
