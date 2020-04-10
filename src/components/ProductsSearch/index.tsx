99d417ec50cd0c1649a7fb907fa398ef6b42fdd6
Filipe Macedo <devmacedo@gmail.com>
2020-04-10 17:36:12 -0300

    style: format ProductsSearch code

--- src/components/ProductsSearch/index.tsx
+++ src/components/ProductsSearch/index.tsx
@@ -4,26 +4,28 @@ import SearchIcon from '../../assets/icons/ic_Search.png';
 import './products-search.styles.scss';
 
 interface ProductsSearchProps {
-	onSubmit(query: string): void;
+  onSubmit(query: string): void;
 }
 
 const ProductsSearch: React.FC<ProductsSearchProps> = ({ onSubmit }) => {
-	const [ search, setSearch ] = useState<string>("");
+  const [search, setSearch] = useState<string>('');
 
-	return (
-		<form
-			className="products-search"
-			onSubmit={(e) => {
-				e.preventDefault();
-				onSubmit(search);
-			}}
-		>
-			<input onChange={({ target }) => setSearch(target.value)} placeholder="Nunca dejes de buscar" />
-			<button type="submit">
-				<img src={SearchIcon} />
-			</button>
-		</form>
-	);
+  return (
+    <form
+      className="products-search"
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit(search);
+      }}>
+      <input
+        onChange={({ target }) => setSearch(target.value)}
+        placeholder="Nunca dejes de buscar"
+      />
+      <button type="submit">
+        <img src={SearchIcon} />
+      </button>
+    </form>
+  );
 };
 
 export default ProductsSearch;
