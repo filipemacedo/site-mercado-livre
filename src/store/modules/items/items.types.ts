4b42c30924632572f6d6606f85cccab6815aaa16
Filipe Macedo <devmacedo@gmail.com>
2020-04-10 15:58:59 -0300

    feat: create items interfaces

---
+++ src/store/modules/items/items.types.ts
@@ -0,0 +1,46 @@
+export enum ItemsTypeActions {
+	'SEARCH_ITEMS' = '@items/SEARCH_ITEMS',
+	'SEARCH_ITEMS_LOADING' = '@items/SEARCH_ITEMS_LOADING',
+	'SEARCH_ITEMS_FAILED' = '@items/SEARCH_ITEMS_FAILED',
+	'SEARCH_ITEMS_SUCCESS' = '@items/SEARCH_ITEMS_SUCCESS'
+}
+
+export interface AuthorInterface {
+	name: string;
+	lastname: string;
+}
+
+export interface ItemsInterface {
+	id: string;
+	title: string;
+	picture: string;
+	condition: string;
+	free_shiping: boolean;
+	price: {
+		currency: string;
+		amount: number;
+		decimals: number;
+	};
+}
+
+export interface SearchResults {
+	items: ItemsInterface[];
+	categories: string[];
+	author: AuthorInterface;
+}
+
+export interface ItemsState extends SearchResults {
+	loading: boolean;
+	error: boolean;
+	selectedItem?: ItemsInterface & {
+		description: string;
+		sold_quantity: number;
+	};
+}
+
+export interface ItemsReducerAction {
+	type: string;
+	payload: {
+		searchResults: SearchResults;
+	};
+}
