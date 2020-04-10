0eedc4479d778125d67aeb0741eda041ef728f40
Filipe Macedo <devmacedo@gmail.com>
2020-04-10 16:34:32 -0300

    refactor: remove BrowserRouter in the routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-max-depth */
 
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import history from './history';
 import Main from '../pages/Main';
@@ -11,13 +11,11 @@ import Product from '../pages/Product';
 const Routes: React.FC = () => {
   return (
    <ConnectedRouter history={history}>
-    <BrowserRouter>
       <Switch>
         <Route component={Main} exact path="/" />
         <Route component={Product} path="/items/:id" />
         <Route component={SearchResults} path="/items" />
       </Switch>
-    </BrowserRouter>
     </ConnectedRouter>
   );
 };
