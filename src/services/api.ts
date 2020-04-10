f27bad3dae306b4967f139e673fa638c08cb79e3
Filipe Macedo <devmacedo@gmail.com>
2020-04-10 15:59:28 -0300

    refactor: use interfaces defined in the items.types

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,42 +1,19 @@
 import axios, { AxiosResponse } from 'axios';
+import { SearchResults } from '../store/modules/items/items.types';
 
 interface Response<T> {
 	statusCode: number;
 	body: T;
 }
 
-interface AuthorInterface {
-	name: string;
-	lastname: string;
-}
-
-interface ItemsInterface {
-	id: string;
-	title: string;
-	picture: string;
-	condition: string;
-	free_shiping: boolean;
-	price: {
-		currency: string;
-		amount: number;
-		decimals: number;
-	};
-}
-
-interface ItemsResponse {
- author: AuthorInterface;
-	categories: string[];
-	items: ItemsInterface[];
-}
-
 const api = axios.create({
 	baseURL: 'http://localhost:3000/api'
 });
 
-export const searchItems = async (query: string): Promise<ItemsResponse> => {
+export const searchItems = async (query: string): Promise<SearchResults> => {
 	const response: AxiosResponse = await api.get(`/items?q=${query}`);
 
-	const { body }: Response<ItemsResponse> = response.data;
+	const { body }: Response<SearchResults> = response.data;
 
 	return body;
 };
