1790e41a488e704eb73c7ddd199973d02706db30
Filipe Macedo <devmacedo@gmail.com>
2020-04-10 08:16:53 -0300

    test: ensure ProductPicture render with default size

---
+++ src/components/ProductPicture/__tests__/ProductPicture.spec.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import faker from 'faker';
+import ProductPicture from '..';
+
+describe('ProductPicture Component', () => {
+	it('should render ProductPicture with default size', () => {
+		const picture = {
+			src: faker.internet.avatar(),
+			alt: faker.lorem.paragraph()
+		};
+
+		const app = mount(<ProductPicture {...picture} />);
+
+		expect(app.find('.product__picture')).toHaveLength(1);
+		expect(app.find('.product__picture.product__picture--large')).toHaveLength(0);
+		expect(app.find('.product__picture img')).toHaveLength(1);
+		expect(app.find('.product__picture img').props()).toEqual(picture);
+ });
+});
