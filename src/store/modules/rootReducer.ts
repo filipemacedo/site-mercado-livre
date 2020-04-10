5aa7682c6a66cd4ab61f40ceb484812c349b1556
Filipe Macedo <devmacedo@gmail.com>
2020-04-10 16:35:18 -0300

    feat: add ItemsReducer in the createReducer

--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import { History } from 'history';
+import items from './items/items.reducer';
 
-export default (history: History) => combineReducers({
-  router: connectRouter(history),
-});
+export default (history: History) =>
+	combineReducers({
+		router: connectRouter(history),
+		items
+	});
