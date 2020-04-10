ce8df6cfe87095ac6e069b711a005422d12d7324
Filipe Macedo <devmacedo@gmail.com>
2020-04-10 16:33:33 -0300

    refactor: ensure pass the callback to onSearchSubmit

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from '../../components/Header';
 import NavigationBreadcrumb from '../../components/NavigationBreadcrumb';
+import { useDispatch } from 'react-redux';
+import { searchItems } from '../../store/modules/items/items.actions';
 
 interface DefaultLayoutProps {
   categories: string[];
@@ -10,9 +12,15 @@ const DefaultLayout: React.FC<DefaultLayoutProps> = ({
   categories,
   children,
 }) => {
+  const dispatch = useDispatch();
+
+  function onSearchSubmit(query: string) {
+    dispatch(searchItems(query))
+  }
+
   return (
     <>
-      <Header />
+      <Header onSearchSubmit={onSearchSubmit} />
       <main>
         <NavigationBreadcrumb categories={categories} />
         {children}
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import Header from '../../components/Header';
+import { useDispatch } from 'react-redux';
+import { searchItems } from '../../store/modules/items/items.actions';
 
 const Main: React.FC = () => {
-  return <Header />;
+	const dispatch = useDispatch();
+
+	function onSearchSubmit(query: string) {
+		dispatch(searchItems(query));
+	}
+
+	return <Header onSearchSubmit={onSearchSubmit} />;
 };
 
 export default Main;
