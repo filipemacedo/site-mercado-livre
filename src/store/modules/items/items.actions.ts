c41c841111a5c3e5dc698c46b339079508d7ab10
Filipe Macedo <devmacedo@gmail.com>
2020-04-10 23:15:42 -0300

    feat: add actions to dispatch in the items actions

--- src/store/modules/items/items.actions.ts
+++ src/store/modules/items/items.actions.ts
@@ -1,8 +1,14 @@
 import { action } from 'typesafe-actions';
-import { ItemsTypeActions, SearchResults } from './items.types';
+import { ItemsTypeActions, SearchResults, ItemInterface } from './items.types';
 
 export const searchItems = (query: string) => action(ItemsTypeActions.SEARCH_ITEMS, { query });
 export const searchItemsFailed = () => action(ItemsTypeActions.SEARCH_ITEMS_FAILED);
 export const searchItemsLoading = () => action(ItemsTypeActions.SEARCH_ITEMS_LOADING);
 export const searchItemsSuccess = (searchResults: SearchResults) =>
 	action(ItemsTypeActions.SEARCH_ITEMS_SUCCESS, { searchResults });
+
+export const findItem = (itemId: string) => action(ItemsTypeActions.FIND_ITEM, { itemId });
+export const findItemSuccess = (itemResult: ItemInterface) =>
+	action(ItemsTypeActions.SEARCH_ITEMS_SUCCESS, { itemResult });
+export const findItemFailed = () => action(ItemsTypeActions.FIND_ITEM_FAILED);
+export const findItemLoading = () => action(ItemsTypeActions.FIND_ITEM_LOADING);
