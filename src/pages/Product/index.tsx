708895687e8883b532572508b202fb8e14ad595f
Filipe Macedo <devmacedo@gmail.com>
2020-04-12 04:57:54 -0300

    refactor: remove unused module in the Product page

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import { Helmet } from 'react-helmet';
 import ContainerBox from '../../components/ContainerBox';
 import DefaultLayout from '../../layouts/Default';
 import DesiredProduct from '../../components/DesiredProduct';
