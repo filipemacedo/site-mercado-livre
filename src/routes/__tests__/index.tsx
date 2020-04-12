d0cc2ef8b447b9d6f93ef0a32f8be083ae6002dd
Filipe Macedo <devmacedo@gmail.com>
2020-04-12 03:49:06 -0300

    test: ensure components is defined in routes file

---
+++ src/routes/__tests__/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import Routes from '..';
+import store from '../../store';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Routes', () => {
+  it('should define routes', () => {
+    const app = mount(
+      <Provider store={store}>
+        <Routes />
+      </Provider>,
+    );
+
+    expect(app.find('Connect(ConnectedRouterWithContext)')).toBeDefined();
+    expect(app.find('ConnectedRouter')).toBeDefined();
+    expect(app.find('Router')).toBeDefined();
+    expect(app.find('Switch')).toBeDefined();
+  });
+});
