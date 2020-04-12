9c6e9868da8af05af78a57d9611c6217b0f6b347
Filipe Macedo <devmacedo@gmail.com>
2020-04-12 16:45:22 -0300

    feat: create stories of NavigationBreadcrumbPlaceholder component

---
+++ src/stories/8-NavigationBreadcrumbPlaceholder.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import NavigationBreadcrumbPlaceholderComponent from '../components/NavigationBreadcrumbPlaceholder';
+
+export default {
+  title: 'NavigationBreadcrumbPlaceholder',
+  component: NavigationBreadcrumbPlaceholderComponent,
+};
+
+export const Placeholder = () => <NavigationBreadcrumbPlaceholderComponent />;
