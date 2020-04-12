ea03a22f8377e33e202638de5e8ffe9c250e00b0
Filipe Macedo <devmacedo@gmail.com>
2020-04-12 17:33:50 -0300

    test: ensure function getErrorMessage return a message when code is 500

--- src/utils/error-message.spec.ts
+++ src/utils/error-message.spec.ts
@@ -6,5 +6,12 @@ describe('function getErrorMessage', () => {
 
 		expect(value).toBeDefined();
 		expect(typeof value).toBe('string');
+ });
+ 
+ it('should return a message when code is 500', () => {
+		const value = getErrorMessage(500);
+
+		expect(value).toBeDefined();
+		expect(typeof value).toBe('string');
 	});
 });
