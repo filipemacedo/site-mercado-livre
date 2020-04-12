dcec0dcfa3dc9ac3e819d09e3ae70f4a81c597c4
Filipe Macedo <devmacedo@gmail.com>
2020-04-12 00:21:46 -0300

    test: ensure PlaceholderLayout render children in two rows

--- src/components/PlaceholderLayout/__tests__/PlaceholderLayout.spec.tsx
+++ src/components/PlaceholderLayout/__tests__/PlaceholderLayout.spec.tsx
@@ -2,10 +2,24 @@ import React from 'react';
 import { mount } from 'enzyme';
 import PlaceholderLayout from '..';
 
-describe("PlaceholderLayout component", () => {
- it('should render PlaceholderLayout without children', () => {
-  const app = mount(<PlaceholderLayout />);
+describe('PlaceholderLayout component', () => {
+  it('should render PlaceholderLayout without children', () => {
+    const app = mount(<PlaceholderLayout />);
 
-  expect(app.find(".placeholder-layout")).toBeDefined();
- })
-})
+    expect(app.find('.placeholder-layout')).toBeDefined();
+  });
+
+  it('should render PlaceholderLayout with children and 2 rows', () => {
+    const app = mount(
+      <PlaceholderLayout rows={2}>
+        <p>Lorem ipsum</p>
+      </PlaceholderLayout>,
+    );
+
+    expect(app.find('.placeholder-layout')).toBeDefined();
+    expect(app.find('.placeholder-layout p')).toHaveLength(2);
+    expect(app.find('.placeholder-layout p').first().text()).toEqual(
+      'Lorem ipsum',
+    );
+  });
+});
